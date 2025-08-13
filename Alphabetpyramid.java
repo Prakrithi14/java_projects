3c1d838dcfc5d31e555876985646e979bce9d2b9
Prakrithi <154612957+Prakrithi14@users.noreply.github.com>
2025-08-13 20:30:22 +0530

    Add files via upload

---
+++ Alphabetpyramid.java
@@ -0,0 +1,17 @@
+public class Alphabetpyramid {
+    public static void main(String args[]){
+        int n=5;
+        char c='A';
+        int j;
+        for(int i=1;i<=n;i++){
+                for(j=1;j<=i;j++){
+                    System.out.print(c+" ");
+                    c++;
+                }
+                System.out.println();
+            }
+
+        }
+    }
+    
+
