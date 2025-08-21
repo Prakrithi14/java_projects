8b1ef00d87f4ae77b4db5e38f97e34646f730562
Prakrithi <154612957+Prakrithi14@users.noreply.github.com>
2025-08-21 16:16:55 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,12 @@
+public class swap{
+    public static void main(String args[]){
+        int a=20;
+        int temp;
+         int b=40;
+         System.out.println("Numbers before swapping:"+a+"and"+b);
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("Numbers after swapping:"+a+"and"+b);
+    }
+}
